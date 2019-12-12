50b8130f5985fada989ab8ce174b919d3cf195c5
Marijn van Vliet <w.m.vanvliet@gmail.com>
2019-12-12 13:58:07 +0200

    test

--- tests/test_rsa.py
+++ tests/test_rsa.py
@@ -94,3 +94,9 @@ class TestRsaArray:
         data = np.array([[1], [2], [3], [4]])
         model_dsm = np.array([1, 2, 3, 1, 2, 1])
         assert rsa_array(data, model_dsm, data_dsm_metric='euclidean') == 1
+
+    def test_rsa_temp(self):
+        """Test RSA with a temporal searchlight."""
+        data = np.array([[1, 2, 3], [2, 3, 4]])
+        model_dsm = np.array([1, 2])
+        #assert rsa_array(data, model_dsm, temporal_radius=1, data_dsm_metric='euclidean') == 1
