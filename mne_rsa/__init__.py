6cf99738398a61f6b38e0c1ff49e7937ec44fc2a
tshimizu97 <tshimizu.midd@gmail.com>
2020-07-09 09:03:30 +0300

    fix a bug

--- mne_rsa/__init__.py
+++ mne_rsa/__init__.py
@@ -4,4 +4,4 @@ from .sensor_level import rsa_evokeds, rsa_epochs, dsm_evokeds, dsm_epochs
 from .searchlight import searchlight
 from .rsa import rsa, rsa_gen, rsa_array
 from .dsm import compute_dsm, compute_dsm_cv, dsm_array
-from .viz import plot_dsms, plot_dsms_topos
+from .viz import plot_dsms, plot_dsms_topo
