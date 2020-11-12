8080e0f89ef30bee53933af52a67655422849771
Marijn van Vliet <w.m.vanvliet@gmail.com>
2020-11-12 11:21:39 +0200

    Ok, back to work

--- mne_rsa/__init__.py
+++ mne_rsa/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.4'
+__version__ = '0.5dev'
 from .source_level import rsa_stcs, dsm_stcs, rsa_nifti, dsm_nifti
 from .sensor_level import rsa_evokeds, rsa_epochs, dsm_evokeds, dsm_epochs
 from .searchlight import searchlight
