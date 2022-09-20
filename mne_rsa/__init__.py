d8d12210f490731dac3b36487830e8bc15766461
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-09-20 15:48:10 +0100

    Release version 0.8

--- mne_rsa/__init__.py
+++ mne_rsa/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.8dev'
+__version__ = '0.8'
 from .source_level import (rsa_stcs, dsm_stcs, rsa_stcs_rois, rsa_nifti,
                            dsm_nifti)
 from .sensor_level import rsa_evokeds, rsa_epochs, dsm_evokeds, dsm_epochs
