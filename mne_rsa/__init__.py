1b1078057d800afc4472f38d8ade18fefeefae8b
Marijn van Vliet <w.m.vanvliet@gmail.com>
2023-05-22 09:22:53 +0300

    Bump version

--- mne_rsa/__init__.py
+++ mne_rsa/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.8"
+__version__ = "0.9dev"
 from .source_level import rsa_stcs, dsm_stcs, rsa_stcs_rois, rsa_nifti, dsm_nifti
 from .sensor_level import rsa_evokeds, rsa_epochs, dsm_evokeds, dsm_epochs
 from .searchlight import searchlight
